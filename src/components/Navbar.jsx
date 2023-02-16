7c795e247fc7ab663b2f54b8be226841bcf894a6
estebanp2022 <91852753+estebanp2022@users.noreply.github.com>
2023-02-16 16:44:12 -0500

    diff components created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav>
+      <i className="nav-icon" class="fa-solid fa-dog"></i>
+      <p className="brand-name">pupbnb</p>
+    </nav>
+  );
+}
+
+export default Navbar;
