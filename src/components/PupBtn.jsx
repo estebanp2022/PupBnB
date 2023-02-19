cc5cf076605fbc3741387a0dd057bf50ae488e31
estebanp2022 <91852753+estebanp2022@users.noreply.github.com>
2023-02-19 18:29:40 -0500

    btn style

--- src/components/PupBtn.jsx
+++ src/components/PupBtn.jsx
@@ -3,9 +3,9 @@ import dog2 from "../assets/dog2.svg";
 function PupBtn(props) {
   return (
     <div className="svg-container">
-      <button onClick={props.handleClick} className="dog-btn">
+      <button onClick={props.handleClick} className="btn_center">
         <img src={dog2} alt="" className="dog-svg" />
-        <p>Get More Pictures!</p>
+        <p>GET MORE PICTURES!</p>
       </button>
     </div>
   );
